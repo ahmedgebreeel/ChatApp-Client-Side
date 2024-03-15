95d6a4a1d50e58e9feb161fb1242452341284367
ahmedgebreeel <ahmedgebreeeel@gmail.com>
2024-03-15 19:32:40 +0200

    creating chat pages and its components

---
+++ src/app/components/selected-user/selected-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './selected-user.component.html',
+  styleUrl: './selected-user.component.css'
+})
+export class SelectedUserComponent {
+
+}
---
+++ src/app/components/users-panel/users-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-panel.component.html',
+  styleUrl: './users-panel.component.css'
+})
+export class UsersPanelComponent {
+
+}
