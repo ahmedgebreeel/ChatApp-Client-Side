c59dde8639c6991066bc459c2085671e02a9e6a1
salmakhaled6 <salmakhaled6@yahoo.com>
2024-03-16 01:11:24 +0200

    signup validation

--- Galaxy-Talk/src/app/pages/login/login.component.ts
+++ Galaxy-Talk/src/app/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- Galaxy-Talk/src/app/pages/signup/signup.component.ts
+++ Galaxy-Talk/src/app/pages/signup/signup.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AbstractControl, FormBuilder, FormGroupDirective } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterModule],
+  imports: [FormsModule,ReactiveFormsModule,CommonModule ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent  {
+
+
+regValidation = new FormGroup({
+  name: new FormControl("salma" , [Validators.required,Validators.minLength(3)],),
+  email:new FormControl("salma@gmail.com",[Validators.required,Validators.email]),
+  password:new FormControl("",[Validators.required,Validators.minLength(5)])
+})
+
+get nameValid(){
+  return this.regValidation.controls["name"].valid;
+}
+get emailValid(){
+  return this.regValidation.controls["email"].valid;
+}
+get passwordValid()
+{
+  return this.regValidation.controls["password"].valid;
+
+}
+
+Add(){
+  console.log(this.regValidation);
+  
+}
 
 }
