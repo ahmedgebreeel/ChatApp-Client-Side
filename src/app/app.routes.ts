d7791bf28dac73b797b0101eb767f1c32b5ba649
Ahmed Sayed <ahmedsayedteino@gmail.com>
2024-03-16 04:32:12 +0200

    made the user info section in users panel - added the welcome page backround to the assets and styled it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ChatComponent } from './pages/chat/chat.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: SignupComponent},
+    {path: 'welcome', component: WelcomeComponent},
+    {path: 'chat', component: ChatComponent},
 
 ];
--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UsersPanelComponent } from '../../components/users-panel/users-panel.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [UsersPanelComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
