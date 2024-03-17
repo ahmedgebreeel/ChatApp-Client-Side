f5eba32dbd3767137bfdcfe3ecad8cec799868b8
MoustafaAzouz <moustafaazouz123456@gmail.com>
2024-03-17 03:12:35 +0200

    I updated chatbox and input field component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { ChatComponent } from './pages/chat/chat.component';
+import { InputFieldComponent } from './components/input-field/input-field.component';
+import { SelectedUserComponent } from './components/selected-user/selected-user.component';
+import { ChatBoxComponent } from './components/chat-box/chat-box.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
@@ -10,5 +13,10 @@ export const routes: Routes = [
     {path: 'register', component: SignupComponent},
     {path: 'welcome', component: WelcomeComponent},
     {path: 'chat', component: ChatComponent},
+    {path: 'input', component: InputFieldComponent},
+    {path: 'how', component: ChatBoxComponent},
+
+
+
 
 ];
