7847eb03c61a66738b7a0f6e30eed41e618ef56f
ahmedgebreeel <ahmedgebreeeel@gmail.com>
2024-03-18 00:36:28 +0200

    adding toastr to login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,86 +1,89 @@
-
-
 import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import {Router , RouterModule } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { LoginService } from '../../service/login.service';
 import { HttpClientModule } from '@angular/common/http';
 
 import { ToastrService } from 'ngx-toastr';
 
-   @Component({
-   selector: 'app-login',
-    standalone: true,
+@Component({
+  selector: 'app-login',
+  standalone: true,
 
-  imports: [RouterModule,ReactiveFormsModule , NgClass ,CommonModule, HttpClientModule],
-  providers:[LoginService],
+  imports: [
+    RouterModule,
+    ReactiveFormsModule,
+    NgClass,
+    CommonModule,
+    HttpClientModule,
+  ],
+  providers: [LoginService],
 
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  changeType: boolean = true;
+  visible: boolean = true;
 
-  changeType:boolean=true;
-  visible :boolean=true;
-
-  constructor(private LService: LoginService , private router :Router,private toastr: ToastrService) {}
+  constructor(
+    private LService: LoginService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ///--------------------- showPassword---------
-    showPassword(){
-      this.changeType= !this.changeType;
-      this.visible = ! this.visible ;
+  showPassword() {
+    this.changeType = !this.changeType;
+    this.visible = !this.visible;
   }
 
-
- // #region for  loginValidation
+  // #region for  loginValidation
   loginValidation = new FormGroup({
-    email : new FormControl("" , [Validators.required,Validators.email]),
-    password : new FormControl("" , [Validators.required , Validators.minLength(8)])
-  })
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
 
-  get EmailValid (){
+  get EmailValid() {
     return this.loginValidation.controls['email'].valid;
   }
 
-  get PassValid (){
-
+  get PassValid() {
     return this.loginValidation.controls['password'].valid;
   }
 
-  Add(email: any, password: any){
-    if(this.loginValidation.valid){
-      console.log("login successfully");
+  Add(email: any, password: any) {
+    if (this.loginValidation.valid) {
 
-    }else{
-      this.toastr.error('Your email or password is wrong');
-
-    }
-    this.LService.login(email, password).subscribe({
-      next:(data)=>{
-        console.log(data);
-        this.toastr.success('You logged successfully');
-        setTimeout(() => {
+      this.LService.login(email, password).subscribe({
+        next: (data) => {
+          console.log(data);
           this.router.navigate(['/welcome']);
-        }, 1000); 
+          this.toastr.success('You logged successfully');
+        },
+        error: (error) => {
+          console.error(error.message);
+          this.toastr.error(' Please try again later.','An error occurred');
+          // this.toastr.error(error.message);
+        },
+      });
 
+    } 
+    else {
+      this.toastr.error('Your email or password is wrong');
       
+    }
 
-      },
-
-
-
-      error: (error) => {
-        console.error(error);
-        this.toastr.error(' Please try again later.','An error occurred');
-      }
-
-  });
   }
 
   // #endregion
-
-
-
 }
-      
