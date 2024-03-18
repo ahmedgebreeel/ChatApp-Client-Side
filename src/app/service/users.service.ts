b9bdbc14b0f6e4064b47729958caf38b7187ec04
ahmedgebreeel <ahmedgebreeeel@gmail.com>
2024-03-18 20:04:25 +0200

    adding selectUser method

--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,10 @@ export class UsersService {
 
   constructor(private client: HttpClient) { }
   private URL = "http://localhost:8000/user";
-  getUsers(){
-    return this.client.get(this.URL);
+  getUsers(token:any):Observable<HttpResponse<any>>{
+    const headers = new HttpHeaders({
+      "Authorization": `Bearer ${token}`
+    })
+    return this.client.get(this.URL, {headers, observe:"response"});
   }
 }
