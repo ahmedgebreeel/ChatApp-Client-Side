b594d9c292d6573bc2b1061bdc1228afbcd1017b
Ahmed Sayed <ahmedsayedteino@gmail.com>
2024-03-19 03:17:45 +0200

    handled the message component - images updated

--- src/app/components/chat-box/chat-box.component.ts
+++ src/app/components/chat-box/chat-box.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MessageComponent } from '../message/message.component';
 
 @Component({
   selector: 'app-chat-box',
   standalone: true,
-  imports: [],
+  imports: [MessageComponent],
   templateUrl: './chat-box.component.html',
   styleUrl: './chat-box.component.css'
 })
