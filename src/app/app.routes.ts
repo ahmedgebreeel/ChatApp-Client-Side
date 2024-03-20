89f53152620ce604f23b2b51abf004b1d21cd915
MoustafaAzouz <moustafaazouz123456@gmail.com>
2024-03-20 03:46:16 +0200

    adding Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { ChatComponent } from './pages/chat/chat.component';
 import { GroupsComponent } from './components/groups/groups.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: SignupComponent},
-    {path: 'welcome', component: WelcomeComponent},
-    {path: 'chat', component: ChatComponent},
-    {path: 'groups', component: GroupsComponent},
+    {path: 'welcome', component: WelcomeComponent,canActivate:[authGuard]},
+    {path: 'chat', component: ChatComponent,canActivate:[authGuard]},
+    {path: 'groups', component: GroupsComponent,canActivate:[authGuard]},
 ];
