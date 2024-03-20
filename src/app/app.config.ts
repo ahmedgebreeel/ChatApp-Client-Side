5c7514811fb2c62860b47cf929745241205cd2a2
ahmedgebreeel <ahmedgebreeeel@gmail.com>
2024-03-20 15:23:21 +0200

    modifying user avater

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,9 +7,10 @@ import { provideToastr } from 'ngx-toastr';
 import { authInterceptor } from './service/interceptor/auth.interceptor';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),provideClientHydration(), provideAnimationsAsync(), provideToastr(), provideHttpClient(withInterceptorsFromDi()),  
+  providers: [provideRouter(routes),provideClientHydration(),provideAnimations(), provideAnimationsAsync(), provideToastr(), provideHttpClient(withInterceptorsFromDi()),  
     {
         provide:HTTP_INTERCEPTORS,
         useClass:authInterceptor,
